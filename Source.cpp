707913c5cffa904f5c0a7b9aae4de4a74a5ab789
t-hunsberger <t-hunsberger@onu.edu>
2024-04-11 08:49:21 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+const int ARRAY_SIZE = 10;
+const int MIN_VALUE = 1000;
+const int MAX_VALUE = 9999;
+
+int main() {
+    vector<int> array(ARRAY_SIZE);
+
+    // Seed the random number generator
+    srand(time(nullptr));
+
+    // Initialize array with random 4-digit integers
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        array[i] = rand() % (MAX_VALUE - MIN_VALUE + 1) + MIN_VALUE;
+    }
+
+    // Print every element in order of appearance
+    cout << "The random integers: ";
+    for (int num : array) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    // Print every element at an even index
+    cout << "Even indices:        ";
+    for (int i = 0; i < ARRAY_SIZE; i += 2) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    // Print every even element
+    cout << "Even values:         ";
+    for (int num : array) {
+        if (num % 2 == 0) {
+            cout << num << " ";
+        }
+    }
+    cout << endl;
+
+    // Print all elements in reverse order
+    cout << "Reversed order:      ";
+    for (int i = ARRAY_SIZE - 1; i >= 0; i--) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+
+    // Print only the first and last element
+    cout << "First and last:      " << array.front() << " " << array.back() << endl;
+
+    return 0;
+}
